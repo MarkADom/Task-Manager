d61a9953541a9a4195b479e9f96fd67849e90d87
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-23 23:41:49 +0000

    TaskStatusEnum

--- src/main/java/com/marco/RestApiWithSpringBoot/entities/Task.java
+++ src/main/java/com/marco/RestApiWithSpringBoot/entities/Task.java
@@ -1,5 +1,6 @@
 package com.marco.RestApiWithSpringBoot.entities;
 
+import com.marco.RestApiWithSpringBoot.entities.enums.TaskStatusEnum;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
---
+++ src/main/java/com/marco/RestApiWithSpringBoot/entities/enums/TaskStatusEnum.java
@@ -0,0 +1,8 @@
+package com.marco.RestApiWithSpringBoot.entities.enums;
+
+public enum TaskStatusEnum {
+    PENDING,
+    IN_PROGRESS,
+    DONE,
+    KILLED
+}
