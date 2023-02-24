5354cbc4fa3476feae7bc25e1040a21994aaa6a4
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-24 00:49:48 +0000

    TaskController Update

--- src/main/java/com/marco/RestApiWithSpringBoot/controllers/TaskController.java
+++ src/main/java/com/marco/RestApiWithSpringBoot/controllers/TaskController.java
@@ -5,9 +5,10 @@ import com.marco.RestApiWithSpringBoot.requests.CreateTaskInput;
 import com.marco.RestApiWithSpringBoot.services.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
@@ -23,6 +24,49 @@ public class TaskController {
         Task taskCreated = taskService.create(createTaskInput.toTask());
 
         return new ResponseEntity<>(taskCreated, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> allTasks() {
+        List<Task> tasks = taskService.findAll();
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> oneTask(@PathVariable int id) {
+        Optional<Task> optionalTask = taskService.findById(id);
+
+        if (optionalTask.isPresent()) {
+            return new ResponseEntity<>(optionalTask.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PatchMapping("/tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable int id, @RequestBody UpdateTaskInput updateTaskInput) {
+        Optional<Task> optionalTask = taskService.findById(id);
+
+        if (optionalTask.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Task taskToUpdate = optionalTask.get();
+
+        taskToUpdate.setStatus(updateTaskInput.status());
+        taskToUpdate.setDueDate(updateTaskInput.dueDate());
+
+        Task taskUpdated = taskService.update(taskToUpdate);
+
+        return new ResponseEntity<>(taskUpdated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int id) {
+        taskService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
