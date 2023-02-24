88e5ed2349eaa5a9478f707217cb0d110f2f4432
Marco Domingues <marco.a.dom78@gmail.com>
2023-02-24 00:00:30 +0000

    TaskController implementation

---
+++ src/main/java/com/marco/RestApiWithSpringBoot/controllers/TaskController.java
@@ -0,0 +1,15 @@
+package com.marco.RestApiWithSpringBoot.controllers;
+
+import com.marco.RestApiWithSpringBoot.services.TaskService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+
+    public TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+}
